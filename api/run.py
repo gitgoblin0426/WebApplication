2236654fa33dadaa9bc786dd1152931f064d4aa7
untamokallio0426 <untamokallio950426@outlook.com>
2021-01-03 23:51:56 +0200

    Add Store Procedure and POST Method to API

--- api/run.py
+++ api/run.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_restful import Api
+from flask_restful import Api,reqparse
 from views import Club
+from flask import jsonify, request
+
+
+from db import Db
+
+from sqlalchemy import text as sql_text
+
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
 
-api.add_resource(Club, '/')
+api.add_resource(Club, '/clubs',methods=['POST', 'GET'])
 
-if __name__ == "__main__":
-    app.run()
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0')
+
--- db/init.sql
+++ db/init.sql
@@ -2,19 +2,43 @@ CREATE TABLE `club` (
     `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
     `club` VARCHAR(512) NOT NULL,
     `club_country` VARCHAR(100),
-    `club_points` VARCHAR(100),
     `created` DATETIME DEFAULT CURRENT_TIMESTAMP(),
     `updated` DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP()
 );
 
-INSERT INTO `club` (`club`, `club_country`, `club_points`)
-VALUES ('TSV 1860 Munich', 'Deutschland', '7');
+INSERT INTO `club` (`club`, `club_country`)
+VALUES ('TSV 1860 Munich', 'Deutschland');
 
-INSERT INTO `club` (`club`, `club_country`, `club_points`)
-VALUES ('VfR Aalen', 'Deutschland', '5');
+INSERT INTO `club` (`club`, `club_country`)
+VALUES ('VfR Aalen', 'Deutschland');
 
-INSERT INTO `club` (`club`, `club_country`, `club_points`)
-VALUES ('VfL 07 Bremen', 'Deutschland', '4');
+INSERT INTO `club` (`club`, `club_country`)
+VALUES ('VfL 07 Bremen', 'Deutschland');
 
-INSERT INTO `club` (`club`, `club_country`, `club_points`)
-VALUES ('Blau-Weiss 1890 Berlin', 'Deutschland', '3');
+INSERT INTO `club` (`club`, `club_country`)
+VALUES ('Blau-Weiss 1890 Berlin', 'Deutschland');
+
+CREATE TABLE `match` (
+    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `comment` VARCHAR(500),
+    `date` DATETIME DEFAULT CURRENT_TIMESTAMP()
+);
+
+CREATE TABLE `club_match` (
+    `idclub_match` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `idclub` INT(11) NOT NULL,
+    `idmatch` INT(11) NOT NULL,
+    `points` INT(11) NOT NULL,  
+    `created` DATETIME DEFAULT CURRENT_TIMESTAMP(),
+    `updated` DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(),
+    FOREIGN KEY (idclub) REFERENCES `club`(id),
+    FOREIGN KEY (idmatch) REFERENCES `match`(id));
+
+-- CREATE PROCEDURE
+DELIMITER //
+CREATE PROCEDURE `matchClubs`(IN `id1` INT(11), IN `point1` INT(11), IN `id2` INT(11), IN `point2` INT) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN INSERT INTO `match` (`comment`) VALUES(''); SET @last_id = LAST_INSERT_ID(); INSERT INTO `club_match` (`idmatch`,`idclub`, `points`) VALUES(@last_id, id1, point1); INSERT INTO `club_match` (`idmatch`,`idclub`, `points`) VALUES(@last_id, id2, point2); END //
+
+-- CALL STORE PROCEDURE
+CALL matchClubs(1,3,2,0);
+
+
